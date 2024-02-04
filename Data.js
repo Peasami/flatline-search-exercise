8be234bd0467c98198d0e28ca3d7d95f92c8eb59
Santeri <122432859+Peasami@users.noreply.github.com>
2024-02-04 16:55:55 +0200

    initial commit

---
+++ Data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+    {
+        id: '1',
+        firstname: 'Matti',
+        lastname: 'Juntunen',
+    },
+    {
+        id: '2',
+        firstname: 'Pekka',
+        lastname: 'Ojala',
+    },
+    {
+        id: '3',
+        firstname: 'Teemu',
+        lastname: 'Koskinen',
+    },
+    {
+        id: '4',
+        firstname: 'Jari',
+        lastname: 'Korhonen',
+    },
+    {
+        id: '5',
+        firstname: 'Kari',
+        lastname: 'Virtanen',
+    },
+    {
+        id: '6',
+        firstname: 'Jussi',
+        lastname: 'Mäkinen',
+    },
+    {
+        id: '7',
+        firstname: 'Jukka',
+        lastname: 'Eronen',
+    },
+    {
+        id: '8',
+        firstname: 'Jani',
+        lastname: 'Tolonen',
+    },
+    {
+        id: '9',
+        firstname: 'Mikko',
+        lastname: 'Karjalainen',
+    },
+    {
+        id: '10',
+        firstname: 'Jari',
+        lastname: 'Uusitalo',
+    },
+    {
+        id: '11',
+        firstname: 'Mikko',
+        lastname: 'Rantanen',
+    },
+    {
+        id: '12',
+        firstname: 'Jari',
+        lastname: 'Laine',
+    },
+    {
+        id: '13',
+        firstname: 'Matti',
+        lastname: 'Heikkinen',
+    },
+    {
+        id: '14',
+        firstname: 'Jukka',
+        lastname: 'Koski',
+    },
+    {
+        id: '15',
+        firstname: 'Jari',
+        lastname: 'Hämäläinen',
+    },
+]
